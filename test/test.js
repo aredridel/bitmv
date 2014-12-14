4af22ccd396166cf39ba4d6866aefa2776f35259
Aria Stewart <aredridel@nbtsc.org>
2014-12-13 19:55:57 -0500

    Fix speling of binmv to bitmv in tests

--- test/test.js
+++ test/test.js
@@ -1,17 +1,17 @@
 var test = require('tape');
-var binmv = require('../index.js');
+var bitmv = require('../index.js');
 
 test('transitive_closure', function (t) {
-    var mtx = binmv.fromBinStrings(['101', '000', '010']);
-    binmv.transitiveClosure(mtx);
-    t.ok(binmv.get(mtx, 0, 0));
-    t.ok(binmv.get(mtx, 0, 1));
-    t.ok(binmv.get(mtx, 0, 2));
-    t.ok(!binmv.get(mtx, 1, 0));
-    t.ok(!binmv.get(mtx, 1, 1));
-    t.ok(!binmv.get(mtx, 1, 2));
-    t.ok(!binmv.get(mtx, 2, 0));
-    t.ok(binmv.get(mtx, 2, 1));
-    t.ok(!binmv.get(mtx, 2, 2));
+    var mtx = bitmv.fromBinStrings(['101', '000', '010']);
+    bitmv.transitiveClosure(mtx);
+    t.ok(bitmv.get(mtx, 0, 0));
+    t.ok(bitmv.get(mtx, 0, 1));
+    t.ok(bitmv.get(mtx, 0, 2));
+    t.ok(!bitmv.get(mtx, 1, 0));
+    t.ok(!bitmv.get(mtx, 1, 1));
+    t.ok(!bitmv.get(mtx, 1, 2));
+    t.ok(!bitmv.get(mtx, 2, 0));
+    t.ok(bitmv.get(mtx, 2, 1));
+    t.ok(!bitmv.get(mtx, 2, 2));
     t.end();
 });
